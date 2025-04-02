5535b8f4c1e3877923d65bfab7525e3a662b95eb
Preston-Franklin-student <prfr981@student.oaisd.org>
2025-04-02 14:41:03 -0400

    Created a basic level and item generator.

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LastJourney
+{
+    public class LevelGenerator : MonoBehaviour
+    {
+        public GameObject generator;
+        public GameObject ground;
+        public GameObject clock;
+        // Start is called before the first frame update
+        void Start()
+        {
+            StartCoroutine(GenerateLevel());
+        }
+
+        IEnumerator GenerateLevel()
+        {
+            int columnHeight = Random.Range(1, 4);
+            int columnAmount = 0;
+            int maxColumnAmount = Random.Range(2, 7); ;
+            int xposition = 0;
+            int yposition = -1;
+
+            int itemGenerator = 0;
+
+            for(int x = 0; x < 100; x++)
+            {
+                if (columnAmount == maxColumnAmount)
+                {
+                    columnHeight = Random.Range(1, 4);
+                    columnAmount = 0;
+                    maxColumnAmount = Random.Range(2, 7);
+                }
+                else
+                {
+                    columnAmount += 1;
+                }
+
+                for (int i = 0; i < columnHeight; i++)
+                {
+                    yposition += 1;
+                    generator.transform.position = new Vector2(xposition, yposition);
+                    Instantiate(ground, transform.position, transform.rotation);
+                }
+                itemGenerator = Random.Range(1, 6);
+                if (itemGenerator == 1)
+                {
+                    yposition += 1;
+                    generator.transform.position = new Vector2(xposition, yposition);
+                    Instantiate(clock, transform.position, transform.rotation);
+                }
+
+                yposition = -1;
+                xposition += 1;
+                yield return new WaitForSeconds(0.01f);
+            }
+        }
+    }
+}
